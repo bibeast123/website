9a55d0ccb5c9fc9183d4037ab5f3b7e1b905663d
Bidhan Devkota <bidhan.devkota@gmail.com>
2024-06-24 20:06:04 -0500

    google analystics tag

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactGA.initialize('G-NE5VHDJXF8');
+ReactGA.pageview('https://bidhandev.me/');
+
 root.render(
   <React.StrictMode>
     <App />
